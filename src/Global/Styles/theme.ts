734056e28f47297017bbc0c27eb86087078f9be8
GarciaaMarilia <garciaamarilia@gmail.com>
2022-01-27 10:23:18 -0300

    feat: colors and fonts used in the application

---
+++ src/Global/Styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    colors: {
+        paleBlue: '#02E3E5',
+        gray: '#231A2B',
+        orange: '#EE540C',
+        white: '#FFFFFF'
+    },
+
+    fonts: {
+        title: 'Poppins_700Bold',
+        subtitle: 'Poppins_300Light'
+    }
+}
