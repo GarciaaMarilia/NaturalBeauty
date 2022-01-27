a9a494b60c4a8cec05793682e1a9f28a131233ab
GarciaaMarilia <garciaamarilia@gmail.com>
2022-01-27 10:16:58 -0300

    feat: Component Background pale blue used in the application

---
+++ src/Components/Background/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+import { theme } from '../../Global/theme';
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: theme.colors.paleBlue,
+        flex: 1,
+    }
+})
